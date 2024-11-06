97f0509e8f1b28d4576c42cc1717a61f48ee75da
D1093N3 <remidalpe@hotmail.com>
2024-11-06 08:50:33 -0500

    v0.1.0-alpha6

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './src/scripts/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: 'development', // or 'development'
 };
